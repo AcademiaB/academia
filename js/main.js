e4c17cce966a66401e0bdf86ba91591717953434
mayomayo25 <10bvb25hal45016@gmail.com>
2016-12-05 15:25:04 +0900

    [update] onSlideLeaveのテスト成功

--- js/main.js
+++ js/main.js
@@ -11,7 +11,22 @@ function fullpage_load(){
   $('#fullpage').fullpage({
     anchors: ['aiueo','page_1', 'page_2', 'page_3', 'page_4'],
     menu: '#menu',
-    sectionsColor: ['#FFF','#addbff', '#ff8e88', '#ffc489', '#84ff84']
+    sectionsColor: ['#FFF','#addbff', '#ff8e88', '#ffc489', '#84ff84'],
+
+    //  スライド移動を押した際に読み込みが行われる
+    onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
+      var leavingSlide = $(this);
+
+      //  例:) 3つめのセクション かつ 最初のスライド かつ 右にスライドした場合
+      if(index == 3 && slideIndex == 0 && direction == 'right'){
+        alert("2枚目のスライドへ！！！");
+      }
+
+      //  例:) 3つめのセクション かつ 2枚目のスライド かつ 左にスライドした場合
+      if(index == 3 && slideIndex == 1 && direction == 'left'){
+        alert("最初のスライドへ！！！");
+      }
+    }
   });
 
   //$('#header').fadeIn("slow");
