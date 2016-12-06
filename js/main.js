1b433413f260de02b0996f27d2b31f591ad921df
mayomayo25 <10bvb25hal45016@gmail.com>
2016-12-06 16:56:19 +0900

    [update] 右スライドでおこJKをフェードイン(詳細あり)
    fullpage.jsのcallbackについてはfullpage.jsのGitHubを参照
    https://github.com/alvarotrigo/fullPage.js#callbacks

--- js/main.js
+++ js/main.js
@@ -13,18 +13,72 @@ function fullpage_load(){
     menu: '#menu',
     sectionsColor: ['#FFF','#addbff', '#ff8e88', '#ffc489', '#84ff84'],
 
-    //  スライド移動を押した際に読み込みが行われる
+    afterResize: function(){
+      var pluginContainer = $(this);
+      alert("メニュー読み込みました！！！");
+    },
+
+    //  現在表示しているコンテンツからスクロールし、移動した際
+    onLeave: function(index, nextIndex, direction){
+      var leavingSection = $(this);
+
+      //after leaving section 2
+      if(index == 1 && nextIndex == 3 && direction =='down'){
+        alert("メニューから2店舗目へ");
+        $("#section3 .fp-tableCell").addClass("js_d_none");
+        $("#slide3_1 .fp-tableCell").fadeIn(3000);
+        $("#slide3_1 .fp-tableCell").removeClass("js_d_none");
+      }
+    },
+
+    //  スライド移動を押した際
     onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
       var leavingSlide = $(this);
 
-      //  例:) 3つめのセクション かつ 最初のスライド かつ 右にスライドした場合
-      if(index == 3 && slideIndex == 0 && direction == 'right'){
-        alert("2枚目のスライドへ！！！");
+      //  以下右方向への移動に関する条件
+      //  3つめのセクション かつ 1枚目のスライド かつ 次のスライドが2枚目 かつ 右にスライドした場合
+      if(index == 3 && slideIndex == 0 && nextSlideIndex == 1 && direction == 'right'){
+        alert("右に移動して2枚目のスライドへ！！！");
+        $("#slide3_1 .fp-tableCell").addClass("js_d_none");
+        $("#slide3_2 .fp-tableCell").fadeIn(3000);
+        $("#slide3_1 .fp-tableCell").fadeOut(100);
+        $("#slide3_2 .fp-tableCell").removeClass("js_d_none");
       }
 
-      //  例:) 3つめのセクション かつ 2枚目のスライド かつ 左にスライドした場合
-      if(index == 3 && slideIndex == 1 && direction == 'left'){
-        alert("最初のスライドへ！！！");
+      //  3つめのセクション かつ 2枚目のスライド かつ 次のスライドが3枚目 かつ 右にスライドした場合
+      if(index == 3 && slideIndex == 1 && nextSlideIndex == 2 && direction == 'right'){
+        alert("右に移動して3枚目のスライドへ！！！");
+        $("#slide3_2 .fp-tableCell").addClass("js_d_none");
+        $("#slide3_3 .fp-tableCell").fadeIn(3000);
+        $("#slide3_2 .fp-tableCell").fadeOut(100);
+        $("#slide3_3 .fp-tableCell").removeClass("js_d_none");
+      }
+
+      //  3つめのセクション かつ 3枚目のスライド かつ 次のスライドが4枚目 かつ 右にスライドした場合
+      if(index == 3 && slideIndex == 2 && nextSlideIndex == 3 && direction == 'right'){
+        alert("右に移動して4枚目のスライドへ！！！");
+        $("#slide3_3 .fp-tableCell").addClass("js_d_none");
+        $("#slide3_4 .fp-tableCell").fadeIn(3000);
+        $("#slide3_3 .fp-tableCell").fadeOut(100);
+        $("#slide3_4 .fp-tableCell").removeClass("js_d_none");
+      }
+
+      //  3つめのセクション かつ 4枚目のスライド かつ 次のスライドが5枚目 かつ 右にスライドした場合
+      if(index == 3 && slideIndex == 3 && nextSlideIndex == 4 && direction == 'right'){
+        alert("右に移動して5枚目のスライドへ！！！");
+        $("#slide3_4 .fp-tableCell").addClass("js_d_none");
+        $("#slide3_5 .fp-tableCell").fadeIn(3000);
+        $("#slide3_4 .fp-tableCell").fadeOut(100);
+        $("#slide3_5 .fp-tableCell").removeClass("js_d_none");
+      }
+
+      //  3つめのセクション かつ 5枚目のスライド かつ 次のスライドが1枚目 かつ 右にスライドした場合
+      if(index == 3 && slideIndex == 4 && nextSlideIndex == 0 && direction == 'left'){
+        alert("右に移動して1枚目のスライドへ！！！");
+        $("#slide3_5 .fp-tableCell").addClass("js_d_none");
+        $("#slide3_1 .fp-tableCell").fadeIn(3000);
+        $("#slide3_5 .fp-tableCell").fadeOut(100);
+        $("#slide3_1 .fp-tableCell").removeClass("js_d_none");
       }
     }
   });
